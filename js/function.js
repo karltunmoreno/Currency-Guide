e2da60cfe6ac763b8deb6e72c88f037d5199253c
Karltun Moreno (javacode.crypto) <95730440+karltunmoreno@users.noreply.github.com>
2022-02-08 00:59:23 -0500

    Going live

---
+++ js/function.js
@@ -0,0 +1,44 @@
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
+
+from_currencyEl.addEventListener('change', calculate);
+from_ammountEl.addEventListener('input', calculate);
+to_currencyEl.addEventListener('change', calculate);
+to_ammountEl.addEventListener('input', calculate);
+
+exchange.addEventListener('click', () => {
+    //const temp = from_currencyEl.value;
+    //from_currencyEl.value = to_currencyEl.value;
+    //to_currencyEl.value = temp;
+    calculate();
+});
+
+
+
+function calculate() {
+    //  API WORKS, take entire function and update HTML elements appropraitely..
+    const from_currency = from_currencyEl.value;
+    const to_currency = to_currencyEl.value;
+
+    fetch('https://v6.exchangerate-api.com/v6/56d17cce079bf5c4493a2b66/pair/USD/' + to_currency)
+        .then(res => (res.json()))
+        .then(data => { 
+             console.log(data)
+             const conversion =  from_ammountEl.value * data.conversion_rate;
+             to_ammountEl.value = conversion;
+             })
+
+
+    // .then(res => {
+    //const rate = res.rates[to_currency];
+    //rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
+    //to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+    //})//
+}
+
+
+calculate();
